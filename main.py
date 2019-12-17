530b4e2f4f372d566470c1a0403009e35624e7a7
Lorenzo <adreani117@gmail.com>
2019-12-17 12:58:08 +0100

    Add some things

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 import sys
 import os
-from shutil import copyfile
+from prettytable import PrettyTable
 
 def main():
     print('Welcome to text classifier main program!')
     print('developed by Lorenzo Adreani')
 
     print('args: ', str(sys.argv))
+    loadOperations()
+
+def loadOperations():
     needToBeSplitted = False
     datasetPath = ""
-
     for arg in sys.argv:
         if arg == '-s':
             needToBeSplitted = True
-        if arg == '-d':
+        elif arg == '-d':
             datasetPath = sys.argv[sys.argv.index(arg) + 1]
+        elif arg == '--help':
+            printHelp()
+            return
+    importData(datasetPath, needToBeSplitted)
 
-    print('You selected the path', datasetPath)
+def importData(datasetPath, needToBeSplitted):
+    # os.mkdir("data")
 
-def importData(datasetPath):
-    os.mkdir("data")
-    copyfile()
+    x = PrettyTable()
+    x.field_names = ["Category", "#dirs"]
 
+    totData = 0
 
+    for dir in os.listdir(datasetPath):
+        if os.path.isdir(datasetPath + dir):
+            nData = len(os.listdir(datasetPath + dir))
+            totData += nData
+            x.add_row([dir, str(nData)])
+    
+    print(x)
+    print("Dimension of dataset(in files):" + str(totData))
+
+def printHelp():
+    print("Questo e' il menu di help")
 
 main()
